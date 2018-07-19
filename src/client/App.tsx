930c8fe4e84359f89dcea8b075a45ae6eb25e429
zmzhang <ziming.zhang@yitu-inc.com>
2018-07-19 22:12:09 +0800

    add layout

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,11 +2,9 @@ import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 import { Provider } from 'react-redux';
 import { IntlProvider } from 'react-intl';
-// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import store from './store';
 import Router from './router';
-// import muiTheme from './muiTheme';
 
 class App extends Component<any, any> {
     render() {
